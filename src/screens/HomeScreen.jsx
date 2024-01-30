62affb84fe49452904bc7269943e0bfa355f63c8
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-01-30 17:54:31 +0000

    Added axios and fetch all and single product from backend

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,7 +1,20 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Product from '../components/Product';
-import products from '../products';
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data);
+    };
+  
+    fetchProducts();
+  }, []);
+
+
   return (
     <>
       <h1 className="text-3xl font-bold my-4 mt-6">Latest Products</h1>
--- src/screens/ProductScreen.jsx
+++ src/screens/ProductScreen.jsx
@@ -1,10 +1,20 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import { useParams, Link } from 'react-router-dom';
 import Rating from '../components/Rating';
-import products from '../products';
 
 const ProductScreen = () => {
+  const [product, setProduct] = useState({});
   const { id: productId } = useParams();
-  const product = products.find((p) => p._id === productId);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+    };
+  
+    fetchProduct();
+  }, [productId]);
 
   return (
     <>
