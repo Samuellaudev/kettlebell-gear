4d4858b8e29d72748d30def810e9e47b8512c64f
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-01-30 12:10:51 +0000

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslintPlugin from 'vite-plugin-eslint';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    eslintPlugin({
+      // setup the plugin
+      cache: false,
+      include: ['./src/**/*.js', './src/**/*.jsx'],
+      exclude: [],
+    }),
+  ],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5000',
+    },
+  },
+});
