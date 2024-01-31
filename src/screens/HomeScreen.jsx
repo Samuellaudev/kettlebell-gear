de90b832a8d113b7690d6154dd0d94e83c2a5112
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-01-31 01:43:55 +0000

    Added Products API Slice & “Get Products” Endpoint

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,32 +1,29 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useGetProductsQuery } from '../slices/productsApiSlice';
 import Product from '../components/Product';
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
-      setProducts(data);
-    };
-  
-    fetchProducts();
-  }, []);
-
+  const { data: products, isLoading, error } = useGetProductsQuery();
 
   return (
     <>
-      <h1 className="text-3xl font-bold my-4 mt-6">Latest Products</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4">
-        {products.map((product) => (
-          <div key={product._id}>
-            <Product product={product} />
+      { isLoading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>{ error?.data.message || error.error }</div>
+      ) : (
+        <>
+          <h1 className="text-3xl font-bold my-4 mt-6">Latest Products</h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4">
+            { products.map((product) => (
+              <div key={ product._id }>
+                <Product product={ product } />
+              </div>
+            )) }
           </div>
-        ))}
-      </div>
+        </>)
+      }
     </>
-  );
+  )
 };
 
 export default HomeScreen;
