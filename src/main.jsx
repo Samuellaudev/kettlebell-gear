c5c260e5fbc57ce3989eab5d086f9b75e19f27f2
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-01-31 01:30:09 +0000

    Added Redux and Redux-Toolkit; implemented store and related slices and reducer logic

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import App from './App.jsx'
 import HomeScreen from './screens/HomeScreen';
@@ -23,6 +25,8 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={ router } />
+    <Provider store={ store }>
+      <RouterProvider router={ router } />
+    </Provider>
   </React.StrictMode>,
 )
---
+++ src/utils/constants.jsx
@@ -0,0 +1,4 @@
+export const BASE_URL = ''; // If using proxy
+
+export const PRODUCTS_URL = '/api/products';
+
