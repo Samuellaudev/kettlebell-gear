76042aa2bb2cd8e45da4e7966aa7209aecafbf64
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-02-05 14:05:41 +0000

    Modified UserListScreen and OrderListScreen

--- src/screens/admin/OrderListScreen.jsx
+++ src/screens/admin/OrderListScreen.jsx
@@ -2,63 +2,89 @@ import { Link } from 'react-router-dom';
 import { FaTimes } from 'react-icons/fa';
 import Message from '../../components/Message';
 import Loader from '../../components/Loader';
+import Paginate from '../../components/Paginate';
 import { useGetOrdersQuery } from '../../slices/ordersApiSlice';
 
 const OrderListScreen = () => {
   const { data: orders, isLoading, error } = useGetOrdersQuery();
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-semibold mb-4">Orders</h1>
+    <div className="container px-4 py-8 mx-auto">
+      <div className="sm:flex sm:items-center sm:justify-between">
+        <h2 className="text-lg font-medium text-gray-800 ">Orders</h2>
+      </div>
+
       {isLoading ? (
         <Loader />
       ) : error ? (
-        <Message variant='danger'>
-          {error?.data?.message || error.error}
-        </Message>
+        <Message variant='error'>{error?.data.message}</Message>
       ) : (
-        <table className='table-auto w-full'>
-          <thead>
-            <tr>
-              <th className="px-4 py-2">ID</th>
-              <th className="px-4 py-2">USER</th>
-              <th className="px-4 py-2">DATE</th>
-              <th className="px-4 py-2">TOTAL</th>
-              <th className="px-4 py-2">PAID</th>
-              <th className="px-4 py-2">DELIVERED</th>
-              <th className="px-4 py-2"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {orders.map((order) => (
-              <tr key={order._id}>
-                <td className="border px-4 py-2">{order._id}</td>
-                <td className="border px-4 py-2">{order.user && order.user.name}</td>
-                <td className="border px-4 py-2">{order.createdAt.substring(0, 10)}</td>
-                <td className="border px-4 py-2">${order.totalPrice}</td>
-                <td className="border px-4 py-2">
-                  {order.isPaid ? (
-                    order.paidAt.substring(0, 10)
-                  ) : (
-                    <FaTimes className="text-red-500 mx-auto" />
-                  )}
-                </td>
-                <td className="border px-4 py-2">
-                  {order.isDelivered ? (
-                    order.deliveredAt.substring(0, 10)
-                  ) : (
-                    <FaTimes className="text-red-500 mx-auto" />
-                  )}
-                </td>
-                <td className="border px-4 py-2">
-                  <Link to={`/order/${order._id}`} className="text-blue-500 hover:underline">
-                    Details
-                  </Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <>
+        <div className="flex flex-col mt-6 px-4 md:px-0">
+          <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+              <div className="overflow-hidden border border-gray-200 md:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-200">
+                  <thead className="bg-gray-50">
+                    <tr>
+                      <th scope="col" className="py-3.5 px-4 text-sm font-normal text-center rtl:text-right text-gray-500">
+                        ID
+                      </th>
+                      <th scope="col" className="px-12 py-3.5 text-sm font-normal text-center rtl:text-right text-gray-500">
+                        USER
+                      </th>
+                      <th scope="col" className="px-4 py-3.5 text-sm font-normal text-center rtl:text-right text-gray-500">
+                        DATE
+                      </th>
+                      <th scope="col" className="px-4 py-3.5 text-sm font-normal text-center rtl:text-right text-gray-500">
+                        TOTAL
+                      </th>
+                      <th scope="col" className="px-4 py-3.5 text-sm font-normal text-center rtl:text-right text-gray-500">
+                        PAID
+                      </th>
+                      <th scope="col" className="px-4 py-3.5 text-sm font-normal text-center rtl:text-right text-gray-500">
+                        DELIVERED
+                      </th>
+                      <th scope="col" className="px-4 py-3.5 text-sm font-normal text-center rtl:text-right text-gray-500">
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="bg-white divide-y divide-gray-200">
+                    {orders.map((order) => (
+                      <tr key={order._id}>
+                        <td className="px-4 py-4 text-sm text-gray-500 whitespace-nowrap">{order._id}</td>
+                        <td className="px-4 py-4 text-sm text-gray-500 whitespace-nowrap">{order.user && order.user.name}</td>
+                        <td className="px-4 py-4 text-sm text-gray-500 whitespace-nowrap">{order.createdAt.substring(0, 10)}</td>
+                        <td className="px-4 py-4 text-sm text-gray-500 whitespace-nowrap">${order.totalPrice}</td>
+                        <td className="px-4 py-4 text-sm text-gray-500 whitespace-nowrap">
+                          {order.isPaid ? (
+                            order.paidAt.substring(0, 10)
+                          ) : (
+                            <FaTimes className="text-red-500 mx-auto" />
+                          )}
+                        </td>
+                        <td className="px-4 py-4 text-sm text-gray-500 whitespace-nowrap">
+                          {order.isDelivered ? (
+                            order.deliveredAt.substring(0, 10)
+                          ) : (
+                            <FaTimes className="text-red-500 mx-auto" />
+                          )}
+                        </td>
+                        <td className="px-4 py-4 text-sm text-gray-500 whitespace-nowrap">
+                          <Link to={`/order/${order._id}`} className="text-blue-500 hover:underline">
+                            Details
+                          </Link>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Paginate pages={ orders.pages } page={ orders.page } isAdmin={ true } />
+      </>
       )}
     </div>
   );
--- src/screens/admin/UserListScreen.jsx
+++ src/screens/admin/UserListScreen.jsx
@@ -1,7 +1,10 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom';
 import { FaTrash, FaEdit, FaCheck, FaTimes } from 'react-icons/fa';
 import Message from '../../components/Message';
 import Loader from '../../components/Loader';
+import Paginate from '../../components/Paginate';
+import Modal from '../../components/Modal';
 import {
   useDeleteUserMutation,
   useGetUsersQuery,
@@ -11,72 +14,124 @@ import { toast } from 'react-toastify';
 const UserListScreen = () => {
   const { data: users, refetch, isLoading, error } = useGetUsersQuery();
 
+  const [isOpen, setIsOpen] = useState(false);
+  const [dialog, setDialog] = useState({})
+
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+  
+  const handleDeleteClick = (user) => {
+    setDialog({
+      title: `Are you sure you want do delete '${user.name}'?`,
+      handleYesClick: () => deleteHandler(user),
+      yesButtonText: 'Yes',
+      handleNoClick: closeModal,
+      noButtonText: 'No',
+    })
+    openModal()
+  }
+
   const [deleteUser] = useDeleteUserMutation();
 
-  const deleteHandler = async (id) => {
-    if (window.confirm('Are you sure')) {
+  const deleteHandler = async (user) => {
       try {
-        await deleteUser(id);
+        await deleteUser(user._id);
         refetch();
+
+        closeModal()
         toast.success('User deleted successfully')
       } catch (err) {
         toast.error(err?.data?.message || err.error);
       }
-    }
   };
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-semibold mb-4">Users</h1>
-      {isLoading ? (
-        <Loader />
-      ) : error ? (
-        <Message variant='error'>
-          {error?.data?.message || error.error}
-        </Message>
-      ) : (
-        <table className="w-full border-collapse table-auto">
-          <thead>
-            <tr>
-              <th className="px-4 py-2">ID</th>
-              <th className="px-4 py-2">NAME</th>
-              <th className="px-4 py-2">EMAIL</th>
-              <th className="px-4 py-2">ADMIN</th>
-              <th className="px-4 py-2"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user) => (
-              <tr key={user._id}>
-                <td className="border px-4 py-2">{user._id}</td>
-                <td className="border px-4 py-2">{user.name}</td>
-                <td className="border px-4 py-2">
-                  <a href={`mailto:${user.email}`}>{user.email}</a>
-                </td>
-                <td className="border px-4 py-2">
-                  {user.isAdmin ? (
-                    <FaCheck className="text-green-500 mx-auto" />
-                  ) : (
-                    <FaTimes className="text-red-500 mx-auto" />
-                  )}
-                </td>
-                <td className="border px-4 py-2 flex space-x-4 justify-center items-center">
-                  <Link to={`/admin/user/${user._id}/edit`} className="btn btn-light mr-2">
-                    <FaEdit className='my-[0.5rem]'/>
-                  </Link>
-                  <button
-                    className="btn btn-danger"
-                    onClick={() => deleteHandler(user._id)}
-                  >
-                    <FaTrash className='text-xl p-1 bg-red-500 text-white rounded-sm'/>
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-    </div>
+    <>
+      <Modal
+        isOpen={isOpen}
+        openModal={openModal}
+        closeModal={closeModal}
+        dialog={ dialog }
+        handleYesClick={dialog.handleYesClick}
+        handleNoClick={dialog.handleNoClick}
+      />
+      <div className="container px-4 py-8 mx-auto">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <h2 className="text-lg font-medium text-gray-800 ">Users</h2>
+        </div>
+        {isLoading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant='error'>{error?.data.message}</Message>
+        ) : (
+        <>
+          <div className="flex flex-col mt-6 px-4 md:px-0">
+            <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+              <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                <div className="overflow-hidden border border-gray-200 md:rounded-lg">
+                  <table className="min-w-full divide-y divide-gray-200">
+                    <thead className="bg-gray-50">
+                      <tr>
+                        <th scope="col" className="py-3.5 px-4 text-sm font-normal text-center rtl:text-right text-gray-500">
+                          ID
+                        </th>
+                        <th scope="col" className="px-12 py-3.5 text-sm font-normal text-center rtl:text-right text-gray-500">
+                          NAME
+                        </th>
+                        <th scope="col" className="px-4 py-3.5 text-sm font-normal text-center rtl:text-right text-gray-500">
+                          EMAIL
+                        </th>
+                        <th scope="col" className="px-4 py-3.5 text-sm font-normal text-center rtl:text-right text-gray-500">
+                          REGISTRATION DATE
+                        </th>
+                        <th scope="col" className="px-4 py-3.5 text-sm font-normal text-center rtl:text-right text-gray-500">
+                          ADMIN
+                        </th>
+                        <th scope="col" className="px-4 py-3.5 text-sm font-normal text-center rtl:text-right text-gray-500">
+                          ACTIONS
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody className="bg-white divide-y divide-gray-200">
+                      {users.map((user) => (
+                        <tr key={user._id}>
+                          <td className="px-4 py-4 text-sm text-gray-500 whitespace-nowrap">{user._id}</td>
+                          <td className="px-4 py-4 text-sm text-gray-500 whitespace-nowrap">{user.name}</td>
+                          <td className="px-4 py-4 text-sm text-gray-500 whitespace-nowrap">
+                            <a href={`mailto:${user.email}`}>{user.email}</a>
+                          </td>
+                          <td className="px-4 py-4 text-sm text-gray-500 whitespace-nowrap">{user.createdAt.substring(0, 10)}</td>
+                          <td className="px-4 py-4 text-sm text-gray-500 whitespace-nowrap">
+                            {user.isAdmin ? (
+                              <FaCheck className="text-green-500 mx-auto" />
+                            ) : (
+                              <FaTimes className="text-red-500 mx-auto" />
+                            )}
+                          </td>
+                          <td className="px-4 py-4 text-sm text-gray-500 whitespace-nowrap flex space-x-4 justify-center items-center">
+                            <Link to={`/admin/user/${user._id}/edit`} className="btn btn-light mr-2">
+                              <FaEdit className='my-[0.5rem]'/>
+                            </Link>
+                            <button
+                              className="btn btn-danger"
+                              onClick={() => handleDeleteClick(user)}
+                            >
+                              <FaTrash className='text-xl p-1 bg-red-500 text-white rounded-sm'/>
+                            </button>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Paginate pages={ users.pages } page={ users.page } isAdmin={ true } />
+        </>
+        )}
+      </div>
+    </>
   );
 };
 
