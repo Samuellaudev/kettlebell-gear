4a0e7412aec48f9324f3f15e3771631387f0488e
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-02-07 03:36:24 +0000

    Fix bug: prevent user from accessing private routes even if no cookie with a valid JWT

--- src/slices/usersApiSlice.jsx
+++ src/slices/usersApiSlice.jsx
@@ -8,6 +8,7 @@ export const usersApiSlice = apiSlice.injectEndpoints({
         url: `${USERS_URL}/login`,
         method: 'POST',
         body: data,
+        credentials: 'include',
       }),
     }),
     logout: builder.mutation({
